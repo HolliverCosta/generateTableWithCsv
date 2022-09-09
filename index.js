eb887e0503cca4741b495f39012d7cad79940b81
HolliverCosta <holliver.costa@ccc.ufcg.edu.br>
2022-09-09 14:29:00 -0300

    finalizado

--- index.js
+++ index.js
@@ -4,46 +4,64 @@ const db = require('./database/index');
 const fastCsv = require('fast-csv');
 
 const multer = require('multer');
+const { query } = require('express');
+const path = require('path');
+const fs = require('fs');
 
-const upload = multer({ dest: 'uploads/' })
+const upload = multer({storage: multer.diskStorage({
+  destination: path.resolve(__dirname,'uploads'),
+  filename: (req, file, callback) => {
+    const fileName = file.originalname;
+    return callback(null, fileName);
+  },
+})});
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
 app.post('/dados/:label', upload.single('csv'), async (req, res) => {
   const { label }  = req.params;
+  const { file } = req;
 
-  var retorno = [];
-  
-  fastCsv.parseFile("municipios.csv",{
+  var csv = [];
+
+  fastCsv.parseFile(file.path,{
     headers: true,
     delimiter: ';'
   })
   .on('data', data => {
-    retorno.push(data)
+    csv.push(data);
   }).on('end', async ()=>{
-    const colunas = Object.keys(retorno[0]);
-    console.log(colunas)
-    var query= ''; 
+    const colunas = Object.keys(csv[0]);
+
+    var createQuery= ''; 
     
     colunas.map((coluna)=>{
-      query = query + coluna + ' VARCHAR' + ','
-    })
+      createQuery = createQuery + coluna + ' VARCHAR' + ','
+    });
+
     await db.query(`
-      CREATE TABLE IF NOT EXISTS ${label}(${query.substring(0,query.length-1)});
-    `)
-    
-    retorno.map(async values => {
-      await db.query(`
-      INSERT INTO ${label}(${colunas})
-      VALUES($1, $2, $3, $4, $5, $6)
-      RETURNING *;
-    `, [values.exibicao,values.regiao,values.uf,values.municipio,values.latitude,values.longitude])
-    })
-    
+      CREATE TABLE IF NOT EXISTS ${label}(${createQuery.substring(0,createQuery.length-1)});
+    `);
+
+    var query = '';
+    csv.map(async values => {
+      var value = '';
+
+      Object.values(values).map((i)=> {
+        value = `${value},'${i}'`
+      });
+
+      query = query + `INSERT INTO ${label}(${colunas})
+      VALUES(${value.substring(1, value.length)});`
+    });
+    await db.query(query);
+  }).on('close', () => {
+    fs.unlinkSync(file.path)
+
   })
 
-res.send('ok').status(200)
+  res.send('Tabela criada!').status(200);
 })
 
-app.listen(8080, () => console.log("esta rodando na porta http://localhost:8080/"))
+app.listen(8080, () => console.log("esta rodando na porta http://localhost:8080/"));
